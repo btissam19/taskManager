3d77f98d25d8c9bdfb437415e52f3db2abeb2207
Ibtissam <btissam1234btissam12@gmail.com>
2023-08-27 21:57:54 +0100

    home page changing

--- src/app.js
+++
@@ -1,7 +0,0 @@
-///############ include important directory#####################
-const express = require("express")
-const app=express()
-const home=require('../router/home')
-app.use('/','home')
-
-app.listen(port,connect=>{console.log(`you are connect in succusfully`)})
